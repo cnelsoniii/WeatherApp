d1daf8a131caa76b246ace564cb571d2c327056e
Cornelius Nelson <corneliusnelson@Corneliuss-MacBook-Air.local>
2020-04-25 16:40:00 -0700

    First commit

---
+++ Weather/WeatherApp/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        //public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/forecast";
+
+        public static string OpenWeatherMapAPIKey = "d5b05b328bf6f45d61514e6403057400";
+    }
+}
---
+++ Weather/WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        RestService _restService;
+        public MainPage()
+        {
+            InitializeComponent();
+            string CurrentDateTime = DateTime.Now.ToString();
+            _restService = new RestService();
+        }
+
+        async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
+            {
+                WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestUri(Constants.OpenWeatherMapEndpoint));
+                BindingContext = weatherData;
+                string weather = weatherData.LocationWeather.Visibility;
+                weatherData.WeatherIcon = WeatherDisplayIcon(weather);
+            }
+        }
+
+        string GenerateRequestUri(string endpoint)
+        {
+            string requestUri = endpoint;
+            requestUri += $"?q={_cityEntry.Text}";
+            requestUri += "&units=imperial"; // or units=metric
+            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+            return requestUri;
+        }
+
+        public ImageSource WeatherDisplayIcon(string weather)
+        {
+            ImageSource image;
+            string source = string.Empty;
+
+            if (weather == "Thunderstorm")
+                source = "Icons/rainthunder.png";
+            else if (weather == "Drizzle" || weather == "Rain")
+                source = "Icons/rain.png";
+            else if (weather == "Snow")
+                source = "Icons/snow.png";
+            else if (weather == "Clear")
+                source = "Icons/clear.png";
+            else if (weather == "Clouds")
+                source = "Icons/cloudy.png";
+            else
+                source = "Icons/warning.png";
+
+            image = ImageSource.FromFile(source);
+            return image;
+        }
+    }
+}
